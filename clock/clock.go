51a070dc106712c1aa52d1e65bdd73408c0f5ee8
Nikola <nikola.paunovic@legal.one>
2020-01-09 22:52:31 +0100

    Clock solution:
    Impresive benchmark :) :
    BenchmarkAddMinutes-8           40413255                27.6 ns/op             0 B/op          0 allocs/op
    BenchmarkSubtractMinutes-8      47937313                21.6 ns/op             0 B/op          0 allocs/op
    BenchmarkCreateClocks-8         168202993                7.24 ns/op            0 B/op          0 allocs/op

--- clock/clock.go
+++ clock/clock.go
@@ -2,60 +2,33 @@ package clock
 
 import "fmt"
 
-const HoursInDay = 24
+const MinutesInDay = 24 * 60
 const MinutesInHour = 60
-const LastHour = 23
 
-// Clock represents clock implementation
-type Clock struct {
-	m int
+// Clock is clock that is very efficient as don't use hours.
+type Clock int
+
+// New creates a new clock.
+func New(h, m int) Clock {
+	minutes := (h*MinutesInHour + m) % MinutesInDay
+	if minutes < 0 {
+		minutes += MinutesInDay
+	}
+	return Clock(minutes)
 }
 
-// New constructor for Clock Type
-func New(h int, m int) Clock {
-	h, m = hourAndMinute(h*MinutesInHour + m)
-	return Clock{h*MinutesInHour + m}
+// String returns a string representation of the clock.
+func (c Clock) String() string {
+	h := c / MinutesInHour
+	return fmt.Sprintf("%02d:%02d", h, c-h*MinutesInHour)
 }
 
-// Add method adds minutes to current instance
-func (c Clock) Add(a int) Clock {
-	h, m := hourAndMinute(c.m + a)
-	c.m = h*MinutesInHour + m
-	return c
+// Add creates a new clock that is `minute` ahead from this one.
+func (c Clock) Add(m int) Clock {
+	return New(0, int(c)+m)
 }
 
 // Subtract method subtract minutes to current instance
 func (c Clock) Subtract(s int) Clock {
-	h, m := hourAndMinute(c.m - s)
-	c.m = h*MinutesInHour + m
-	return c
-}
-
-// String stringer interface implementation
-func (c Clock) String() string {
-	h, m := hourAndMinute(c.m)
-	return fmt.Sprintf("%0.2d:%0.2d", h, m)
-}
-
-// hourAndMinute calculate hour and minute based on total number of mintues
-func hourAndMinute(min int) (h, m int) {
-	if min >= 0 {
-		h = (min / MinutesInHour) % HoursInDay
-		m = min % MinutesInHour
-	} else {
-		min = -min
-		h = LastHour - (min/MinutesInHour)%HoursInDay
-		m = MinutesInHour - (min % MinutesInHour)
-	}
-
-	if h == HoursInDay {
-		h = 0
-	}
-
-	if m == MinutesInHour {
-		m = 0
-		h = (h + 1) % HoursInDay
-	}
-
-	return
+	return New(0, int(c)-s)
 }
